108fd61de513637a73b1a4d14a8b355fc857b209
Manish Sharma <mrsharma98@gmail.com>
2020-09-20 02:02:46 +0530

    Added Buttons

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Button.module.css'
+
+const button = (props) => (
+    <button
+        className={[classes.Button, classes[props.btnType]].join(' ')}
+        // we are using [] bcz we have to give multiple classes and dynamically
+        // then joining and making them a string separated by a wide space
+        onClick={props.clicked}>{props.children}</button>
+)
+
+export default button;
