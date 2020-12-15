cca21e619234531c00e4b0be0f788d1078da5eba
Manish Sharma <mrsharma98@gmail.com>
2020-12-15 17:27:08 +0530

    applied saga to burger builder and Auth

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,7 +2,9 @@ import { takeEvery } from 'redux-saga/effects'
 // it listen to certain action and do something when they occured
 
 import * as actionTypes from '../actions/actionTypes'
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from './auth'
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth'
+import { initIngredientsSaga } from './burgerBuilder'
+
 
 export function* watchAuth () {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
@@ -10,4 +12,10 @@ export function* watchAuth () {
     // logoutSaga will execute whenever we come cross the mentioned actionTypes
 
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
-}   
+    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+
+}
+
+export function* watchBurgerBuilder () {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
+}
